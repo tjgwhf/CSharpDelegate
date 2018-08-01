5af7c4a86ceb85de2f1c04dc15591bf450592e6d
Noma <1181098979@qq.com>
2018-08-01 16:47:32 +0800

    Delegate
    Delegate

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_委托 {
+    class Program
+    {
+        static void Sort(int[] sortArray)
+        {
+            bool swapped = true;
+            do
+            {
+                swapped = false;
+                for (int i = 0; i < sortArray.Length - 1; i++)
+                {
+                    if (sortArray[i] > sortArray[i + 1])
+                    {
+                        int temp = sortArray[i];
+                        sortArray[i] = sortArray[i + 1];
+                        sortArray[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+            
+        }
+
+        static void CommonSort<T>(T[] sortArray,Func<T,T,bool> compareMethod)
+        {
+            bool swapped = true;
+            do
+            {
+                swapped = false;
+                for (int i = 0; i < sortArray.Length - 1; i++)
+                {
+                    if (compareMethod(sortArray[i],sortArray[i+1]))
+                    {
+                        T temp = sortArray[i];
+                        sortArray[i] = sortArray[i + 1];
+                        sortArray[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+
+        }
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[]
+            {
+
+            }
+        }
+    }
+}
